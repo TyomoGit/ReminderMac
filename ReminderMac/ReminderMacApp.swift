c276fef215255263a661df6feb5435c254b4e79a
TyomoGit <riku3dno.1@gmail.com>
2022-10-12 22:56:40 +0900

    Initial Commit

---
+++ ReminderMac/ReminderMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderMacApp.swift
+//  ReminderMac
+//
+//  Created by Tyomo on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct ReminderMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
